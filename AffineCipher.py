3abe0b4770d86a2fa6015aadbe07290b1f160346
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-02-02 18:11:04 +0300

    Done with Affine recurrent cipher.

--- AffineCipher.py
+++ AffineCipher.py
@@ -1,4 +1,5 @@
 from random import randint, choice
+from collections import deque
 
 import helpers as hps
 
@@ -14,9 +15,9 @@ class AffineCipher:
             self.key_a = choice([x for x in range(2, MOD) if hps.gcd(MOD, x) == 1])
         if self.key_b is None:
             self.key_b = randint(0, MOD - 1)  # random num from [0, MOD)
-        self.__get_decryption_key__()
+        self.decryption_key_a = self.__get_decryption_key__(self.key_a)
 
-    def __get_decryption_key__(self):
+    def __get_decryption_key__(self, key):
         """ initialize decryption key based on self.key_a and MOD"""
 
         '''
@@ -28,10 +29,10 @@ class AffineCipher:
 
         inv1 = [0]
         inv2 = [0]
-        if hps.gcd_extended(self.key_a, MOD, inv1, inv2) != 1:
+        if hps.gcd_extended(key, MOD, inv1, inv2) != 1:
             raise ValueError
 
-        self.decryption_key_a = inv1[0] % MOD
+        return inv1[0] % MOD
 
     def encrypt(self, text):
         arr = hps.str_to_array(text)
@@ -58,22 +59,46 @@ class AffineRecCipher(AffineCipher):
             self.key_a2 = choice([x for x in range(2, MOD) if hps.gcd(MOD, x) == 1])
         if self.key_b2 is None:
             self.key_b2 = randint(0, MOD - 1)  # random num from [0, MOD)
-        # store initial keys
-        self.initial_keys_a = (self.key_a, self.key_a2)
-        self.initial_keys_b = (self.key_b, self.key_b2)
-        self.iteration = 0  # which pair of keys we want to use
-        self.prev_keys = []
 
-    def generate_new_keys(self):
-        # лучше хранить 2 предыдущие пары ключей
-        if self.iteration == 0:
-            return
-        elif self.iteration == 1:
-            self.key_a,
+    def encrypt(self, plaintext):
+        arr = hps.str_to_array(plaintext)
+        ans = []
+        prev_keys = deque()
+        for ind, char in enumerate(arr):
+            if ind == 0:
+                ans.append((char * self.key_a + self.key_b) % MOD)
+                prev_keys.append((self.key_a, self.key_b))
+            elif ind == 1:
+                ans.append((char * self.key_a2 + self.key_b2) % MOD)
+                prev_keys.append((self.key_a2, self.key_b2))
+            else:
+                old_keys = prev_keys.popleft()
+                new_keys = ((old_keys[0] * prev_keys[-1][0]) % MOD, (old_keys[1] + prev_keys[-1][1]) % MOD)
+                ans.append((char * new_keys[0] + new_keys[1]) % MOD)
+                prev_keys.append(new_keys)
+            print(prev_keys)
+        return hps.arr_to_str(ans)
 
-    def encrypt(self, text):
-        self.generate_new_keys()
-        super().encrypt(text)
+    def decrypt(self, ciphertext):
+        arr = hps.str_to_array(ciphertext)
+        ans = []
+        prev_keys = deque()
+        for ind, char in enumerate(arr):
+            if ind == 0:
+                decryption_key = self.__get_decryption_key__(self.key_a)
+                prev_keys.append((decryption_key, self.key_b))
+                ans.append(((char - self.key_b) * decryption_key) % MOD)
+            elif ind == 1:
+                decryption_key = self.__get_decryption_key__(self.key_a2)
+                prev_keys.append((decryption_key, self.key_b2))
+                ans.append(((char - self.key_b2) * decryption_key) % MOD)
+            else:
+                old_keys = prev_keys.popleft()
+                new_keys = ((old_keys[0] * prev_keys[-1][0]) % MOD, (old_keys[1] + prev_keys[-1][1]) % MOD)
+                ans.append(((char - new_keys[1]) * new_keys[0]) % MOD)
+                prev_keys.append(new_keys)
+
+        return hps.arr_to_str(ans)
 
 
 if __name__ == '__main__':
@@ -81,7 +106,18 @@ if __name__ == '__main__':
     print(ac.key_a)
     print(ac.key_b)
     print(ac.decryption_key_a)
-    s = 'abacaba'
+    s = 'iloveu'
     cs = ac.encrypt(s)
     print(cs)
     print(ac.decrypt(cs))
+    print('---------')
+    arc = AffineRecCipher()
+    print(arc.key_a)
+    print(arc.key_a2)
+    print(arc.key_b)
+    print(arc.key_b2)
+
+    cs = arc.encrypt(s)
+    print(cs)
+
+    print(arc.decrypt(cs))
