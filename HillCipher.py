9e8d24a8603fd916cf31e6b9507f1328144caa63
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-02-16 13:30:28 +0300

    add modular calculation of inverse matrix

--- HillCipher.py
+++ HillCipher.py
@@ -1,4 +1,5 @@
 import numpy as np
+from helpers import gcd_extended
 
 
 def get_random_matrix():
@@ -72,3 +73,37 @@ for chunk in chunks:
 print(y)
 
 print(arr_to_text(y, inv_d))
+
+
+def matrix_minor(arr, i, j):
+    return np.delete(np.delete(arr, i, axis=0), j, axis=1)
+
+
+# function for computing matrix inverse modulo n , input: matrix A and modulo m
+def inv_matrix_mod_n(matrix, mod):
+    n = len(matrix)
+
+    # inverse = (1/detM) * adj(M) ,where adj(M) is adjugate of matrix M
+    det = int(round(np.linalg.det(matrix)) % mod)
+
+    inverse_det = [0]
+    inv2 = [0]
+    if gcd_extended(det, mod, inverse_det, inv2) != 1:
+        print("БЕДА, из-за округления нод не равен 1", det, mod, inverse_det, inv2, matrix)
+        return None
+
+    # calculate adjugate
+    adj = np.zeros((n, n), dtype=int)
+    for i in range(n):  # rows
+        for j in range(n):  # columns
+            # calculate cofactor of A(i,j)
+            minor = matrix_minor(matrix, i, j)
+            # use adj[j][i] - INV !!!
+            adj[j][i] = int((round(
+                np.linalg.det(minor)) % mod))  # определитель минора по модулю
+            if (i + 1 + j + 1) % 2 == 1:
+                adj[j][i] = (-1 * adj[j][i]) % mod  # алг. дополнение
+    return (inverse_det * adj) % mod
+
+
+print(inv_matrix_mod_n(k, 29))
