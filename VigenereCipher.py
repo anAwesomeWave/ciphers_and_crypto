5bb5db6aaff33fa331d135735f898fe2d9b9e780
anawesomeawe <timofey.zubov.05@gmail.com>
2024-03-04 20:02:28 +0300

    Add vigenere ciphere with self gen key by ciphertext.

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -56,17 +56,18 @@ class VigenereTextKey(VigenereCipherBase):
         self.key_letter = key_letter
 
     def get_key_for_text(self, arr: list[int]) -> list[int]:
-        return [self.d[self.key_letter], *arr[1::]]
+        return [self.d[self.key_letter], *arr[:-1]]
 
     def encrypt(self, text: str) -> list[str]:
         """ Returns both ciphertext and generated key."""
         arr = self.text_to_arr(text)
         key = self.get_key_for_text(arr)
         encrypted = []
-
+        print(arr)
+        print(key)
         for i in range(len(arr)):
             encrypted.append(self.inv_d[(arr[i] + key[i]) % self.MOD])
-        return [encrypted, self.arr_to_text(key)]
+        return [''.join(encrypted), self.arr_to_text(key)]
 
     def decrypt(self, key: str, ciphertext: str) -> str:
         arr = self.text_to_arr(ciphertext)
@@ -74,7 +75,7 @@ class VigenereTextKey(VigenereCipherBase):
         decrypted = []
         for i in range(len(arr)):
             decrypted.append(self.inv_d[(arr[i] - key[i]) % self.MOD])
-        return decrypt
+        return ''.join(decrypted)
 
 
 class VigenereCiphertextKey(VigenereCipherBase):
@@ -83,6 +84,28 @@ class VigenereCiphertextKey(VigenereCipherBase):
 
     def encrypt(self, text: str) -> list[str]:
         """ Returns both ciphertext and generated key."""
+        arr = self.text_to_arr(text)
+        encrypted = []
+        prev_key = self.d[self.key_letter]
+        key = [self.key_letter, ]
+        for i in range(len(arr)):
+            new_char = (prev_key + arr[i]) % self.MOD
+            encrypted.append(self.inv_d[new_char])
+            key.append(self.inv_d[new_char])
+            prev_key = new_char
+        print(arr)
+        print(self.text_to_arr(''.join(key)))
+        print(self.text_to_arr(''.join(encrypted)))
+        return [''.join(encrypted), ''.join(key)]
+
+    def decrypt(self, key: str, ciphertext: str):
+        arr = self.text_to_arr(ciphertext)
+        key = self.text_to_arr(key)
+        decrypted = []
+        for i in range(len(arr)):
+            decrypted.append(self.inv_d[(arr[i] - key[i]) % self.MOD])
+        return ''.join(decrypted)
+
 
 if __name__ == '__main__':
     text = 'attackatdawn'
@@ -92,3 +115,15 @@ if __name__ == '__main__':
     enc = v_s.encrypt(text)
     print(enc)
     print(v_s.decrypt(enc))
+
+    v_t = VigenereTextKey('a')
+    enc, key = v_t.encrypt('abacaba')
+    print(enc)
+    print(v_t.decrypt(key, enc))
+
+    v_c = VigenereCiphertextKey('a')
+    enc, key = v_c.encrypt('abacaba')
+
+    print(enc)
+    print(key)
+    print(v_c.decrypt(key, enc))
